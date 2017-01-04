5b5993baa3097677e438e3a5308e7780979894bb
Mariah Martin Shein <mshein@ctn22.ctn22.uwaterloo.ca>
2017-01-04 16:53:20 -0500

    oneStep done

--- daw_mbl_1.py
+++ daw_mbl_1.py
@@ -3,11 +3,12 @@ import modelbasedlearner as mbl
 
 class Agent():
 	def __init__(self):
-		state_dict = {1:[0], 2:[1, 2], 3:[3, 4]}
+		state_dict = {1:[0], 2:[1, 2]}
 		self.ai = mbl.ModelBasedLearner(actions=["left", "right"], states = state_dict)
 		self.lastAction = None
 		self.lastState = None
-		self.lastLevel = None
+		self.numLevels = len(state_dict)
+		self.firstLevel = 1
 		self.currState = 0
 		self.currLevel = 1
 		self.currAction = None
@@ -64,4 +65,89 @@ class Agent():
 			newValue = addNoise
 		return newValue
 
-	# 
+	# this should return the current reward based on the 
+	# action taken in the current state
+	def calcReward(self, currState, currAction):
+		if currState == 0:
+			return 0
+		currProb = random.random()	
+		if currState == 1:
+			if currAction == "left": # choose left (CASE 1)
+				reward = 0 
+				#print currProb, self.case1RewardProb
+				if currProb > self.case1RewardProb:
+					reward = 1
+				return reward
+			elif currAction == "right": # choose right (CASE 2)
+				reward = 0 
+				#print currProb, self.case2RewardProb
+				if currProb > self.case2RewardProb:
+					reward = 1
+				return reward
+			else:
+				print "Something went very wrong with choosing the action: should be either left or right"
+				return None
+		if currState == 2:
+			if currAction == "left": # choose left (CASE 3)
+				reward = 0 
+				#print currProb, self.case3RewardProb
+				if currProb > self.case3RewardProb:
+					reward = 1
+				return reward
+			elif currAction == "right": # choose right (CASE 4) 
+				reward = 0 
+				#print currProb, self.case4RewardProb
+				if currProb > self.case4RewardProb:
+					reward = 1
+				return reward
+			else:
+				print "Something went very wrong with choosing the action: should be either left or right"
+				return None
+
+	def updateRewardProb(self):
+		self.case1RewardProb = self.randomWalk(self.case1RewardProb)
+		self.case2RewardProb = self.randomWalk(self.case2RewardProb)
+		self.case3RewardProb = self.randomWalk(self.case3RewardProb)
+		self.case4RewardProb = self.randomWalk(self.case4RewardProb)
+
+	# calculates the next state probabilistically
+	# (may want to include some way to change these probabilities externally)
+	# the paper does say that this prob was fixed throughout the experiment
+	def calcNextState(self, currState, currAction):
+		nextState = 0
+		if currState == 0:
+			#print "here"
+			if currAction == "left":
+				state1Prob = random.random()
+				if state1Prob > 0.3: # more likely to be state 1
+					nextState = 1
+				else:
+					nextState = 2
+			if currAction == "right":
+				state1Prob = random.random()
+				if state1Prob > 0.7: # more likely to be state 2
+					nextState = 1
+				else:
+					nextState = 2
+		return nextState
+
+	def calcNextState(self):
+		if self.currLevel == self.numLevels:
+			return self.firstLevel
+		else:
+			return self.currLevel + 1
+
+	def oneStep(self):
+		currAction = self.ai.chooseAction(self.currBoardState)
+		nextBoardState = self.calcNextState(self.currBoardState, currAction)
+		self.currReward = self.calcReward(self.currBoardState, currAction)
+		self.updateRewardProb() #bookkeeping step
+		if self.lastAction != None:
+			self.ai.learn(self.lastBoardState, self.lastAction, self.currReward, self.currBoardState, self.currLevel)
+		# more bookkeeping
+		self.lastBoardState = self.currBoardState
+		self.currBoardState = nextBoardState
+		self.currLevel = self.calcNextState()
+		self.lastAction = currAction	
+
+	
